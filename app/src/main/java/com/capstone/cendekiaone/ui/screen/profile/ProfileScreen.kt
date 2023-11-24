05974ddea93d092f08600f6e8147e582b1c66f1e
Muhammad Dila <muhammaddila.all@gmail.com>
2023-11-24 20:11:12 +0700

    Correct Typo

---
+++ app/src/main/java/com/capstone/cendekiaone/ui/screen/profile/ProfileScreen.kt
@@ -0,0 +1,29 @@
+package com.capstone.cendekiaone.ui.screen.profile
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.capstone.cendekiaone.R
+
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(stringResource(R.string.menu_profile))
+    }
+}
+
+@Preview(showBackground = true, device = "id:pixel_4")
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
