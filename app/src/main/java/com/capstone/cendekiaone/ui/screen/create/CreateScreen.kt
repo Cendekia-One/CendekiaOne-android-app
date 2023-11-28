3b964571054ff86b6cf8ba3a5860c78a2152b4cc
azkifairuz <azkiajmal@gmail.com>
2023-11-29 00:35:09 +0700

    nitiate glide

--- app/src/main/java/com/capstone/cendekiaone/ui/screen/create/CreateScreen.kt
+++ app/src/main/java/com/capstone/cendekiaone/ui/screen/create/CreateScreen.kt
@@ -1,24 +1,34 @@
 package com.capstone.cendekiaone.ui.screen.create
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.capstone.cendekiaone.R
 
 @Composable
 fun CreateScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(23.dp)
     ) {
-        Text(stringResource(R.string.menu_create))
     }
 }
 
