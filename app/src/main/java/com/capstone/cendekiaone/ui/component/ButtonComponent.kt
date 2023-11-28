12e69bbd5083b8ce45aa3cf3b8bd0d71c41bad3e
azkifairuz <azkiajmal@gmail.com>
2023-11-29 01:54:15 +0700

    fix button component, dynamic color and modifier

--- app/src/main/java/com/capstone/cendekiaone/ui/component/ButtonComponent.kt
+++ app/src/main/java/com/capstone/cendekiaone/ui/component/ButtonComponent.kt
@@ -2,20 +2,34 @@ package com.capstone.cendekiaone.ui.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.capstone.cendekiaone.ui.theme.myFont
+
+
 
 @Composable
 fun ButtonComponent(
     provideText: String,
-    onClick: () -> Unit
+    colors: ButtonColors = ButtonDefaults.filledTonalButtonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary
+    ),
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
 ) {
     Button(
         onClick = { onClick() },
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier,
+        colors = colors
     ) {
         Text(provideText, fontFamily = myFont)
     }
--- app/src/main/java/com/capstone/cendekiaone/ui/screen/create/UploadImageScreen.kt
+++ app/src/main/java/com/capstone/cendekiaone/ui/screen/create/UploadImageScreen.kt
@@ -1,12 +1,25 @@
 package com.capstone.cendekiaone.ui.screen.create
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.capstone.cendekiaone.R
+import com.capstone.cendekiaone.ui.component.ButtonComponent
 
 @Composable
 fun UploadImageScreen(
@@ -18,6 +31,51 @@ fun UploadImageScreen(
             .padding(horizontal = 16.dp),
         contentAlignment = Alignment.Center
     ) {
+        Column(
+            modifier = Modifier
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(
+                16.dp,
+                Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_plus_circle),
+                contentDescription = null
+            )
+            Text(text = stringResource(R.string.text__pilih_sumber))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(
+                    24.dp,
+                    Alignment.CenterHorizontally
+                )
+            ) {
+                ButtonComponent(
+                    provideText = "kamera",
+                    colors = ButtonDefaults.filledTonalButtonColors(
+                        containerColor = Color.White,
+                        contentColor = Color.Black,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
 
+                ) {
+
+                }
+                ButtonComponent(
+                    provideText = "galery"
+                ) {
+
+                }
+            }
+        }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun UploadCameraPrev() {
+    UploadImageScreen()
 }
