f35bd3161b278c00fbd8a108e916b27b4ad42948
azkifairuz <azkiajmal@gmail.com>
2023-12-03 01:14:00 +0700

    create uri to file utils

--- app/src/main/java/com/capstone/cendekiaone/utils/any.kt
+++ app/src/main/java/com/capstone/cendekiaone/utils/any.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.graphics.asImageBitmap
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -38,6 +40,22 @@ fun reduceFileImage(file: File): File {
 fun createTempFiles(context: Context): File {
     val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+fun uriToFile(selectedImg: Uri, context: Context): File {
+
+    val myFile = createTempFiles(context)
+    val contentResolver: ContentResolver = context.contentResolver
+    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) {
+        outputStream.write(buf, 0, len)
+    }
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
 }
 @Composable
 fun Int.toBitmap(context: Context): ImageBitmap {
