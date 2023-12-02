c482f1866fa501d5a1e54f7ce830886787bff353
azkifairuz <azkiajmal@gmail.com>
2023-12-02 19:35:07 +0700

    feat:create baseIconBox

--- app/src/main/java/com/capstone/cendekiaone/ui/screen/profile/EditProfile.kt
+++ app/src/main/java/com/capstone/cendekiaone/ui/screen/profile/EditProfile.kt
@@ -1,9 +1,77 @@
 package com.capstone.cendekiaone.ui.screen.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.bumptech.glide.integration.compose.GlideImage
+import com.capstone.cendekiaone.R
+import com.capstone.cendekiaone.ui.navigation.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditProfile(){
+fun EditProfile(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        stringResource(R.string.edit_profile),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.navigate(Screen.Home.route) },
+                        modifier = modifier
+                            .size(40.dp)
+                    ) {
+                        val icon: Painter = painterResource(id = R.drawable.ic_back)
+                        Icon(
+                            painter = icon,
+                            contentDescription = "Icon Back",
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding)
+        ) {
 
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun EditProfilePrev() {
+    EditProfile(
+        navController = rememberNavController()
+    )
 }
